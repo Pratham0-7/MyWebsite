ac587cdcb8d4a7203a401c8c24559ca58730946e
Pratham0-7 <pratham07603@gmail.com>
2025-07-17 22:24:33 +0530

    made some changes in the app.py and config.py

--- Backend/app.py
+++ Backend/app.py
@@ -82,4 +82,4 @@ def generate():
         return jsonify({"response": "⚠️ OpenRouter response failed."}), 500
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(host="0.0.0.0",port=8000)
--- Backend/config.py
+++ Backend/config.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
-EMAIL_USER = os.getenv("EMAIL_USER")
-EMAIL_PASS = os.getenv("EMAIL_PASS")
-MONGO_URI = os.getenv("MONGO_URI")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+MONGO_URI = os.environ.get("MONGO_URI")
+EMAIL_USER = os.environ.get("EMAIL_USER")
+EMAIL_PASS = os.environ.get("EMAIL_PASS")
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
